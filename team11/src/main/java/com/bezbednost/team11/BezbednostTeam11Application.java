133dec4cb9f4e910f7468b64082bc604ce86428c
senpai132 <acanikolic021@gmail.com>
2021-03-29 22:30:16 +0200

    Initial commit

---
+++ team11/src/main/java/com/bezbednost/team11/BezbednostTeam11Application.java
@@ -0,0 +1,14 @@
+package com.bezbednost.team11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezbednostTeam11Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BezbednostTeam11Application.class, args);
+	}
+
+}
