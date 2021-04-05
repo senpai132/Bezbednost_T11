3bc30b04df613f99b0f4b85e55c3db472f5bbc00
milanjokanovic <milan.jokanovic98@gmail.com>
2021-04-05 22:56:52 +0200

    new admin api created

---
+++ adminaplication/src/main/java/adminapi/adminaplication/AdminaplicationApplication.java
@@ -0,0 +1,13 @@
+package adminapi.adminaplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminaplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminaplicationApplication.class, args);
+	}
+
+}
--- team11/src/main/java/com/bezbednost/team11/repository/CertificateSignRepository.java
+++ team11/src/main/java/com/bezbednost/team11/repository/CertificateSignRepository.java
@@ -2,7 +2,9 @@ package com.bezbednost.team11.repository;
 
 import com.bezbednost.team11.model.CertificateSignRequest;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface CertificateSignRepository extends JpaRepository<CertificateSignRequest, Integer>{
+
 }
