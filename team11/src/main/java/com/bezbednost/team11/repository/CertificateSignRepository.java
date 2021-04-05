e0258c2a2951c8262af2c8846706bcc41a6a04df
senpai132 <acanikolic021@gmail.com>
2021-04-05 20:10:20 +0200

    config

--- team11/src/main/java/com/bezbednost/team11/repository/CertificateSignRepository.java
+++ team11/src/main/java/com/bezbednost/team11/repository/CertificateSignRepository.java
@@ -1,8 +1,9 @@
 package com.bezbednost.team11.repository;
 
 import com.bezbednost.team11.model.CertificateSignRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CertificateSignRepository extends JpaRepository<CertificateSignRequest, Integer>{
+public interface CertificateSignRepository extends JpaRepository<CertificateSignRequest, Integer> {
 }
