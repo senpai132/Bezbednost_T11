32fc56abbe2e70c3d75a06534ea01dbe859b2c5c
milanjokanovic <milan.jokanovic98@gmail.com>
2021-04-12 00:17:32 +0200

    fixed front

--- adminFront/src/app/app.module.ts
+++ adminFront/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AllCertificatesComponent } from './admin/certificates/all-certificates/all-certificates.component';
+import { CertificateRequestsComponent } from './admin/certificates/certificate-requests/certificate-requests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllCertificatesComponent,
+    CertificateRequestsComponent
   ],
   imports: [
     BrowserModule,
