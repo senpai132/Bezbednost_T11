3801bc803cc9b6b062ac6bf12961d6e4a3611857
milanjokanovic <milan.jokanovic98@gmail.com>
2021-06-11 23:15:27 +0200

    added drool rules for more then 3 bad logins

--- bolnicaServer/src/main/java/com/example/bolnicaServer/BolnicaServerApplication.java
+++ bolnicaServer/src/main/java/com/example/bolnicaServer/BolnicaServerApplication.java
@@ -1,13 +1,27 @@
 package com.example.bolnicaServer;
 
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BolnicaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BolnicaServerApplication.class, args);
+	}
+
+	@Bean
+	public KieContainer kieContainer() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks
+				.newKieContainer(ks.newReleaseId("rs.sbnz", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+		KieScanner kScanner = ks.newKieScanner(kContainer);
+		kScanner.start(10_000);
+		return kContainer;
 	}
 
 }
